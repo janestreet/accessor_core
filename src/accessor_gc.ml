917168aef39c386864f4d2dc9f528f7f52698bce
Arseniy Alekseyev <aalekseyev@janestreet.com>
2021-08-13 20:19:15 +0100

    v0.15~preview.125.01+410

--- src/accessor_gc.ml
+++ src/accessor_gc.ml
@@ -2,6 +2,8 @@ open! Core
 open! Import
 
 module Stat = struct
+  [%%if ocaml_version < (4, 12, 0)]
+
   type t = Gc.Stat.t =
     { minor_words : float
     ; promoted_words : float
@@ -21,4 +23,29 @@ module Stat = struct
     ; stack_size : int
     }
   [@@deriving accessors]
+
+  [%%else]
+
+  type t = Gc.Stat.t =
+    { minor_words : float
+    ; promoted_words : float
+    ; major_words : float
+    ; minor_collections : int
+    ; major_collections : int
+    ; heap_words : int
+    ; heap_chunks : int
+    ; live_words : int
+    ; live_blocks : int
+    ; free_words : int
+    ; free_blocks : int
+    ; largest_free : int
+    ; fragments : int
+    ; compactions : int
+    ; top_heap_words : int
+    ; stack_size : int
+    ; forced_major_collections : int
+    }
+  [@@deriving accessors]
+
+  [%%endif]
 end
--- src/accessor_gc.mli
+++ src/accessor_gc.mli
@@ -2,6 +2,8 @@ open! Core
 open! Import
 
 module Stat : sig
+  [%%if ocaml_version < (4, 12, 0)]
+
   type t =
     { minor_words : float
     ; promoted_words : float
@@ -21,5 +23,30 @@ module Stat : sig
     ; stack_size : int
     }
   [@@deriving accessors]
+
+  [%%else]
+
+  type t =
+    { minor_words : float
+    ; promoted_words : float
+    ; major_words : float
+    ; minor_collections : int
+    ; major_collections : int
+    ; heap_words : int
+    ; heap_chunks : int
+    ; live_words : int
+    ; live_blocks : int
+    ; free_words : int
+    ; free_blocks : int
+    ; largest_free : int
+    ; fragments : int
+    ; compactions : int
+    ; top_heap_words : int
+    ; stack_size : int
+    ; forced_major_collections : int
+    }
+  [@@deriving accessors]
+
+  [%%endif]
 end
 with type t := Gc.Stat.t
