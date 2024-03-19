db399086f703219210728c058252d7313c5ec03b
public-release <as-public-release@janestreet.com>
2024-03-19 00:29:52 +0000

    v0.17~preview.129.15+205

--- test/accessor_core_tests.ml
+++ test/accessor_core_tests.ml
@@ -14,7 +14,8 @@ let%expect_test "just showing the example" =
     {|
     ((a 0) (b 1) (c 2) (d 3) (e 4) (f 5) (g 6) (h 7) (i 8) (j 9) (k 10) (l 11)
      (m 12) (n 13) (o 14) (p 15) (q 16) (r 17) (s 18) (t 19) (u 20) (v 21)
-     (w 22) (x 23) (y 24) (z 25)) |}]
+     (w 22) (x 23) (y 24) (z 25))
+    |}]
 ;;
 
 let set_of_string string = Char.Set.of_list (String.to_list string)
@@ -27,12 +28,14 @@ let%expect_test "at_key_seti" =
   [%expect
     {|
     (((0) ()) ((1) ()) ((a) (0)) ((b) (1)) ((c) (2)) ((i) (8)) ((j) (9))
-     ((k) (10)) ((x) (23)) ((y) (24)) ((z) (25))) |}];
+     ((k) (10)) ((x) (23)) ((y) (24)) ((z) (25)))
+    |}];
   print (Accessor.set accessor example ~to_:None);
   [%expect
     {|
     ((d 3) (e 4) (f 5) (g 6) (h 7) (l 11) (m 12) (n 13) (o 14) (p 15) (q 16)
-     (r 17) (s 18) (t 19) (u 20) (v 21) (w 22)) |}]
+     (r 17) (s 18) (t 19) (u 20) (v 21) (w 22))
+    |}]
 ;;
 
 let%expect_test "found_key_seti" =
@@ -41,8 +44,7 @@ let%expect_test "found_key_seti" =
   |> [%sexp_of: ((char * unit) Accessor.Index.t * int) list]
   |> print_s;
   [%expect
-    {|
-    (((a) 0) ((b) 1) ((c) 2) ((i) 8) ((j) 9) ((k) 10) ((x) 23) ((y) 24) ((z) 25)) |}];
+    {| (((a) 0) ((b) 1) ((c) 2) ((i) 8) ((j) 9) ((k) 10) ((x) 23) ((y) 24) ((z) 25)) |}];
   print
     (Accessor.mapi accessor example ~f:(fun [ c ] n ->
        n - (Char.to_int c - Char.to_int 'a')));
@@ -50,5 +52,6 @@ let%expect_test "found_key_seti" =
     {|
     ((a 0) (b 0) (c 0) (d 3) (e 4) (f 5) (g 6) (h 7) (i 0) (j 0) (k 0) (l 11)
      (m 12) (n 13) (o 14) (p 15) (q 16) (r 17) (s 18) (t 19) (u 20) (v 21)
-     (w 22) (x 0) (y 0) (z 0)) |}]
+     (w 22) (x 0) (y 0) (z 0))
+    |}]
 ;;
